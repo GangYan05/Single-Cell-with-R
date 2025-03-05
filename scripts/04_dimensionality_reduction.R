7c33915171970e7cce35fbb7d8dc6204c5267880
Gang Yan <yangangmg@gmail.com>
2025-03-05 14:05:15 -0500

    dimensional reduction

--- scripts/04_dimensionality_reduction.R
+++ scripts/04_dimensionality_reduction.R
@@ -29,28 +29,14 @@ plot(percent.var, log="y", xlab="PC", ylab="Variance explained (%)")
 chosen.pcs <- PCAtools::findElbowPoint(percent.var)
 chosen.pcs
 
+# PCA plot
+plotReducedDim(sce, dimred="PCA", color_by = "level1class")
+
 # Perform t-SNE
 sce <- scater::runTSNE(sce, perplexity = 30)
-tsne_results <- reducedDim(sce, "TSNE")
+# t-SNE plot
+plotReducedDim(sce, dimred="TSNE", color_by = "level1class")
 
 # Perform UMAP
-umap_results <- uwot::umap(t(logcounts(sce)), n_neighbors = 15)
-reducedDim(sce, "UMAP_UWOT") <- umap_results
-
-# Save results
-saveRDS(pca_results, file = "results/dimensionality_reduction/pca_results.rds")
-saveRDS(tsne_results, file = "results/dimensionality_reduction/tsne_results.rds")
-saveRDS(umap_results, file = "results/dimensionality_reduction/umap_results.rds")
-
-# Optionally, create plots for visualization
-pdf("results/dimensionality_reduction/pca_plot.pdf")
-plot(pca_results, main = "PCA Plot")
-dev.off()
-
-pdf("results/dimensionality_reduction/tsne_plot.pdf")
-plot(tsne_results, main = "t-SNE Plot")
-dev.off()
-
-pdf("results/dimensionality_reduction/umap_plot.pdf")
-plot(umap_results, main = "UMAP Plot")
-dev.off()
+sce <- runUMAP(sce)
+plotReducedDim(sce, dimred="UMAP", color_by = "level1class")

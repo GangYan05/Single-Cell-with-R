685697d7f180184790d125ec1c5ba1aa93e8c54e
Gang Yan <yangangmg@gmail.com>
2025-05-08 16:17:32 -0400

    update

--- scripts/02_quality_control.R
+++ scripts/02_quality_control.R
@@ -1,6 +1,7 @@
 # Calculate quality control metrics
 qc_df <- perCellQCMetrics(sce, subsets = list(Mito = is_mito))
 qc_df
+
 # Identify low-quality cells with fixed thresholds (optional)
 qc_size <- qc_df$sum < 1e5
 qc_nexpression <- qc_df$detected < 5e3
@@ -10,7 +11,23 @@ todrop <- qc_size | qc_nexpression | qc_spike | qc_mito
 DataFrame(LibSize=sum(qc_size), NExprs=sum(qc_nexpression), Spike=sum(qc_spike), Mito=sum(qc_mito), total=sum(todrop))
 
 # Adaptively filter out low-quality cells
-filtered <- perCellQCFilters(qc_df ,sub.fields = c("subsets_Mito_percent"))
+tofilter <- quickPerCellQC(qc_df, percent_subsets=c("altexps_ERCC_percent", "subsets_Mito_percent"))
+colSums(as.matrix(tofilter))
+
+
+sce_qc <- addPerCellQC(sce)
+sce_qc
+colData(sce_qc)
+plotColData(sce_qc, x="phenotype", y="sum")
+
+colData(sce)
+plotColData(sce, x="total_counts", y="percent_mito", colour_by="discard") +
+    geom_point() +
+    theme_minimal() +
+    labs(title = "Quality Control Metrics",
+         x = "Total Counts",
+         y = "Percent Mitochondrial")
+filtered <- perCellQCFilters(qc_df, sub.fields = c("subsets_Mito_percent"))
 
 # Subset the SingleCellExperiment object to retain only high-quality cells
 sce <- sce[, !filtered$discard]
